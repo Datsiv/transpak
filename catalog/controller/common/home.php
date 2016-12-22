b68efa77c34ae5136aff1b11dc6cc7fe1ccb7ae2
Nazar2306 <kroxa0623@gmail.com>
2016-12-22 08:54:55 +0200

    slideshow home page and category

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -1,45 +1,39 @@
 <?php
 class ControllerCommonHome extends Controller {
-	public function index() {
-		$this->document->setTitle($this->config->get('config_meta_title'));
-		$this->document->setDescription($this->config->get('config_meta_description'));
-		$this->document->setKeywords($this->config->get('config_meta_keyword'));
-
-		if (isset($this->request->get['route'])) {
-			$this->document->addLink(HTTP_SERVER, 'canonical');
-		}
-
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['column_right'] = $this->load->controller('common/column_right');
-		$data['content_top'] = $this->load->controller('common/content_top');
-		$data['content_bottom'] = $this->load->controller('common/content_bottom');
-		$data['footer'] = $this->load->controller('common/footer');
-		$data['header'] = $this->load->controller('common/header');
+    public function index() {
+        $this->document->setTitle($this->config->get('config_meta_title'));
+        $this->document->setDescription($this->config->get('config_meta_description'));
+        $this->document->setKeywords($this->config->get('config_meta_keyword'));
+        if (isset($this->request->get['route'])) {
+            $this->document->addLink(HTTP_SERVER, 'canonical');
+        }
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['column_right'] = $this->load->controller('common/column_right');
+        $data['content_top'] = $this->load->controller('common/content_top');
+        $data['content_bottom'] = $this->load->controller('common/content_bottom');
+        $data['footer'] = $this->load->controller('common/footer');
+        $data['header'] = $this->load->controller('common/header');
 // start category
-		$this->load->model('catalog/product');
-
-		$data['categories'] = array();
-
-		$categories = $this->model_catalog_category->getCategories(0);
-		foreach ($categories as $category) {
-
-				$data['categories'][] = array(
-					'name'     => $category['name'],
-					'image'    => '/image/'.$category['image'],
-					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
-				);
-			}
-
+        $this->load->model('catalog/product');
+        $data['categories'] = array();
+        $categories = $this->model_catalog_category->getCategories(0);
+        foreach ($categories as $category) {
+            $data['categories'][] = array(
+                'name'     => $category['name'],
+                'image'    => '/image/'.$category['image'],
+                'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+            );
+        }
 // end category
         $this->load->model('catalog/information');
         $information_info = $this->model_catalog_information->getInformation(9);
         $data['descriptionseo'] = html_entity_decode($information_info['description'], ENT_QUOTES, 'UTF-8');
         $data['heading_titleseo'] = $information_info['title'];
-        
-		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/home.tpl')) {
-			$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/common/home.tpl', $data));
-		} else {
-			$this->response->setOutput($this->load->view('default/template/common/home.tpl', $data));
-		}
-	}
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/home.tpl')) {
+            $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/common/home.tpl', $data));
+        } else {
+            $this->response->setOutput($this->load->view('default/template/common/home.tpl', $data));
+        }
+    }
 }
--- catalog/view/theme/default/template/module/slideshow.tpl
+++ catalog/view/theme/default/template/module/slideshow.tpl
@@ -1,16 +1,17 @@
 
   <div class="col-lg-12 text_slader_bydte">
-    <div class="banner-div-main">
-      <div class="desktop-img-div-style">
+    <div class="banner-div-main" >
+      <!--<div class="desktop-img-div-style">
         <img class="" src="../image/catalog/ygolku.png" style="width: 100%" >
       </div>
-      <div class="desktop-text-div-style">
+      <!--<div class="desktop-text-div-style">
           <div class="">
             <span class="text_zah_slader">Будьте уверены! - груз доедет в целости и сохранности!</span>
           </div>
           <div class="">
           </div>
-      </div>
+      </div>-->
+
       <div class="hidden-xs desk_text_div_style_backg">
         <span class="glav_text_span_banner">
           Главное преимущество
@@ -19,18 +20,41 @@
       </div>
     </div>
   </div>
+  <?php if($is_home == 'home'){ ?>
+  <div class="col-lg-12 text_slader_bydte">
+    <div class="banner-div-main">
+      <div class="hidden-xs desk_text_div_style_backg_3" style="border-radius: 8px;">
+        <span class="glav_text_span_banner">
+          Акции!
+        </span>
+        <p class="glav_text_p_banner">При покупке ленты тканой Lash
+          2000 GW 500 PES от 10 бобин
+          стоимость 200 м ленты составит
+          98,90 у.е. </p>
+        <div style="padding-bottom: 75px;padding-left: 5px;">
+          <a href="index.php?route=information/akcii">
+              <button type="button"  class=" btn btn-primary bot_vse_akcii" ">
+                <span class="">Все акции</span>
+              </button>
+          </a>
+      </div>
+      </div>
+    </div>
+  </div>
+  <?php } ?>
+
 <div id="slideshow<?php echo $module; ?>" class="owl-carousel" style="opacity: 1;">
   <?php foreach ($banners as $banner) { ?>
   <div class="item">
     <?php if ($banner['link']) { ?>
     <a href="<?php echo $banner['link']; ?>"><img src="<?php echo $banner['image']; ?>" alt="<?php echo $banner['title']; ?>" class="img-responsive" /></a>
     <?php } else { ?>
-    <img src="<?php echo $banner['image']; ?>" alt="<?php echo $banner['title']; ?>" class="img-responsive img_np_20" />
+    <img src="<?php echo $banner['image']; ?>" alt="<?php echo $banner['title']; ?>" class="img-responsive img_np_20" style="margin: -40px 0 -60px 0;" />
     <?php } ?>
   </div>
   <?php } ?>
 </div>
-<script type="text/javascript"><!--
+<script type="text/javascript">
 $('#slideshow<?php echo $module; ?>').owlCarousel({
 	items: 6,
 	autoPlay: 3000,
